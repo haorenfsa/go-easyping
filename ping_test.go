c566d7ac28a804b189bec7a4d6113f48bdcea483
haorenfsa <haorenfsa@gmail.com>
2019-09-04 02:14:07 +0800

    fix test, cleanup readme

--- ping_test.go
+++ ping_test.go
@@ -16,6 +16,6 @@ func Test_Ping(t *testing.T) {
 }
 
 func Test_AdvancedPing(t *testing.T) {
-	_, err := AdvancedPing(&Options{Address: "192.168.1.100", Timeout: time.Second * 1})
+	_, err := AdvancedPing(&Options{Address: "8.8.8.8", Timeout: time.Second * 1})
 	assert.NoError(t, err)
 }
